52f9c3ad3cec26307fc1c75563f6e1c36c0e7ad3
ashadow22 <ashadow22@gmail.com>
2018-05-04 18:12:32 +0300

    gunicorn commit

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn --config /etc/gunicorn.d/test-wsgi hello:app
